939beb7ff46760e31a38699f5928c74153daa12d
Matthew Koniecko <matthew.koniecko@gmail.com>
2025-02-13 17:36:29 -0500

    Complete TODO's

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -10,6 +10,8 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
+    start_node = &m_Model.FindClosestNode(start_x, start_y);
+    end_node = &m_Model.FindClosestNode(end_x, end_y);
 
 }
 
@@ -20,7 +22,7 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 // - Node objects have a distance method to determine the distance to another node.
 
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
-
+    return node->distance(*end_node);
 }
 
 
@@ -32,7 +34,35 @@ float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
 // - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
 
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
+//   current_node->FindNeighbors();
+//   for (auto node: current_node->neighbors) {
+//     node->parent = current_node;
+//     node->h_value = RoutePlanner::CalculateHValue(node);
+//     node->g_value = current_node->g_value + current_node->distance(*node);
+//     node->visited = true;
+//     open_list.push_back(node);
+//   }
+// }
 
+// A more accurate implementation. Node->visited should not be set true until removed form open_list.
+    current_node->visited = true;
+    current_node->FindNeighbors();
+    for (auto node: current_node->neighbors) {
+      if(std::find(open_list.begin(), open_list.end(), node) != open_list.end()) { 
+        float new_g_value = current_node->g_value + current_node->distance(*node);
+            if (new_g_value < node->g_value) {
+                node->g_value = new_g_value;
+                node->parent = current_node; // Update parent to current_node
+            }
+      }
+      else {
+        node->parent = current_node;
+        node->h_value = RoutePlanner::CalculateHValue(node);
+        node->g_value = current_node->g_value + current_node->distance(*node);
+//         node->visited = true;
+        open_list.push_back(node);
+      }
+    }
 }
 
 
@@ -42,9 +72,14 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 // - Create a pointer to the node in the list with the lowest sum.
 // - Remove that node from the open_list.
 // - Return the pointer.
-
+bool compareByDist(const RouteModel::Node* a, const RouteModel::Node* b) {
+    return (a->h_value + a->g_value) > (b->h_value + b->g_value);
+}
 RouteModel::Node *RoutePlanner::NextNode() {
-
+  std::sort(open_list.begin(), open_list.end(), compareByDist);
+  RouteModel::Node* p = open_list.back();
+  open_list.pop_back();
+  return p;
 }
 
 
@@ -62,6 +97,13 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     std::vector<RouteModel::Node> path_found;
 
     // TODO: Implement your solution here.
+    while(current_node != start_node){
+        distance += current_node->distance(*(current_node->parent));
+        path_found.push_back(*current_node);
+        current_node = current_node->parent;
+    }
+    path_found.push_back(*current_node);
+    std::reverse(path_found.begin(), path_found.end());
 
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;
@@ -80,5 +122,14 @@ void RoutePlanner::AStarSearch() {
     RouteModel::Node *current_node = nullptr;
 
     // TODO: Implement your solution here.
-
+    open_list.push_back(start_node);
+    while(open_list.size() > 0) {
+        current_node = NextNode();
+        if (current_node == end_node) {
+        m_Model.path = ConstructFinalPath(current_node);
+        return;
+        }
+        AddNeighbors(current_node);
+    }
+    std::cout << "No path was found" << std::endl;
 }
